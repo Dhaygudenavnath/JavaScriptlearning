6e77a8db33452ac4b76cd7c84d411f92c7b2c1d6
Dhaygudenavnath <navnathdhaygude448@gmail.com>
2022-12-07 23:48:26 -0800

    variable,assign
    variable,assign

---
+++ 01_variable_assign.js
@@ -0,0 +1,20 @@
+
+
+ var myFullName;
+ myFullName="Navnath Nanaso Dhaygude";
+  console.log(myFullName); 
+
+  var myAge="22";
+  console.log(myAge);
+
+  var myAddress="AT post,Balwadi,sangola  ";
+  console.log(myAddress);
+
+  var pin="413308";
+  console.log(pin);
+
+  var myGender="male";
+  console.log(myGender);
+
+  var we_love="I love only jS";
+  console.log(we_love);
